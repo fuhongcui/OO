b6d7ac1c215b1e7438972cdd7b6c1a99a32a9556
cuifh <cuifh@meixing.com>
2023-03-17 16:50:11 +0800

    frustum init

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,11 @@ const string VERTEX_SHADER =
 "#version 330 core \n"
 "layout (location = 0) in vec3 aPos; \n"
 "layout (location = 1) in vec2 aTextureCoord; \n"
-"uniform mat4 modelMatrix; \n"
-"uniform mat4 viewMatrix; \n"
-"uniform mat4 projectMatrix; \n"
+"uniform mat4 mvp; \n"
 "out vec2 TexCoord;"
 "void main() \n"
 "{ \n"
-"   gl_Position = projectMatrix * viewMatrix * modelMatrix * vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
+"   gl_Position = mvp * vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
 "   TexCoord = aTextureCoord; \n"
 "} \n"
 ;
@@ -45,44 +43,37 @@ void WindowSizeCallback(GLFWwindow* window, int width, int height);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 void UserInput(GLFWwindow *window);
 
-const unsigned int WINDOW_WIDTH = 1280;
-const unsigned int WINDOW_HEIGHT = 640;
-
-struct PointCoord
+static const unsigned int WINDOW_WIDTH = 1280;
+static const unsigned int WINDOW_HEIGHT = 640;
+static float FOV = 30.f;
+struct Point2D
 {
-    PointCoord()
-        :x(0.f), y(0.f), z(0.f)
+    Point2D() = default;
+    Point2D(float x, float y)
+        :x(x),y(y)
     {
 
     }
-    PointCoord(float x, float y, float z)
-        :x(x), y(y), z(z)
+    float x {0.f};
+    float y {0.f};
+};
+
+struct PointCoord
+{
+    PointCoord(float x, float y, float z, float u, float v)
+        :x(x), y(y), z(z), u(u), v(v)
     {
 
     }
     float x {0.f};
     float y {0.f};
     float z {0.f};
-
-};
-struct TextureCoord
-{
-    TextureCoord()
-        :u(0.f), v(0.f)
-    {
-
-    }
-    TextureCoord(float u, float v)
-        :u(u), v(v)
-    {
-
-    }
     float u {0.f};
     float v {0.f};
+
 };
 
 
-static float fov = 45.f;
 
 
 int main(int argc, char* agrv[])
@@ -100,7 +91,7 @@ int main(int argc, char* agrv[])
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, WindowSizeCallback);
-    glfwSetScrollCallback(window, ScrollCallback);
+//    glfwSetScrollCallback(window, ScrollCallback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -166,7 +157,26 @@ int main(int argc, char* agrv[])
     glBindTexture(GL_TEXTURE_2D, 0);
     stbi_image_free(dataTexture2);
 
+//vertex
+    PointCoord lb = {-100, -100, 0, 0, 0};
+    PointCoord rb = {100, -100, 0, 1, 0};
+    PointCoord rt = {100, 100, 0, 1, 1};
+    PointCoord lt = {-100, 100, 0, 0, 1};
 
+    vector<PointCoord> vertex;
+    vertex.reserve(6);
+    vertex.emplace_back(lb);
+    vertex.emplace_back(rb);
+    vertex.emplace_back(rt);
+    vertex.emplace_back(lb);
+    vertex.emplace_back(rt);
+    vertex.emplace_back(lt);
+
+    unsigned int VBOvertex;
+    glGenBuffers(1, &VBOvertex);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOvertex);
+    glBufferData(GL_ARRAY_BUFFER, vertex.size() * sizeof(vertex[0]), (void*)&vertex[0], GL_DYNAMIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     //VAO
     unsigned int VAO;
@@ -181,177 +191,49 @@ int main(int argc, char* agrv[])
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LESS);
-
-        glm::mat4 modelMatrix(1.0);
-        glm::mat4 viewMatrix(1.0);
-        glm::mat4 projectMatrix(1.0);
-#if 0
-            //z
-            glm::vec3 cameraPos = glm::vec3(0.f, -2.f, 2.f);
-            glm::vec3 cameraTarget = glm::vec3(0.f, 0.f, 0.f);
-            glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
-            //x
-            glm::vec3 up = glm::vec3(0.f, 0.f, 1.f);
-            glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-            //y
-            glm::vec3 cameraUP = glm::normalize(glm::cross(cameraDirection, cameraRight));
-
-            viewMatrix = glm::lookAt(cameraPos, cameraTarget, up);
-            projectMatrix = glm::perspective(glm::radians(fov), float(WINDOW_WIDTH / WINDOW_HEIGHT), 0.001f, 10000.f);
-#endif
-
-        float l = 0.f;
-        float r = 0.f + WINDOW_WIDTH;
-        float b = 0.f + WINDOW_HEIGHT;
-        float t = 0.f;
-        float n = 0.5f;
-        float f = 1.f;
-//vertex
-        float vvZ = 0.79f;
-        vector<PointCoord> vertex1 =
-        {
-            {0.f, 0.f,          vvZ},
-            {1280.f, 0.f,       vvZ},
-            {1280.f, 640.f,     vvZ},
-            {0.f, 640.f,        vvZ}
-        };
-
-        float v2Z = 0.8f;
-        vector<PointCoord> vertex2 =
-        {
-            {320.f, 160.f, v2Z},
-            {960.f, 160.f, v2Z},
-            {960.f, 480.f, v2Z},
-            {320.f, 480.f, v2Z}
-        };
-
-        vector<TextureCoord> textureCoord =
-        {
-            {0, 1},
-            {1, 1},
-            {1, 0},
-            {0, 0}
-        };
-//ortho
-        auto Ortho = [l, r, b, t, n, f](float x, float y, float z) -> PointCoord
-        {
-            PointCoord oPoint;
-            float Sx = 2 / (r - l);
-            float Sy = 2 / (t - b);
-            float Sz = 2 / (n - f);
-
-            float Tx = - (r + l) / (r - l);
-            float Ty = - (t + b) / (t - b);
-            float Tz = - (n + f) / (n - f);
-
-            oPoint.x = Sx * x + Tx;
-            oPoint.y = Sy * y + Ty;
-            oPoint.z = Sz * z + Tz;
-            return oPoint;
-        };
-//perspective
-        auto Perspective = [l, r, b, t, n, f](float Xe, float Ye, float Ze) -> PointCoord
-        {
-            PointCoord oPoint;
-
-            float Xp = n * Xe / -Ze;
-            float Yp = n * Ye / -Ze;
-
-            //Xp -> Xn : l -> -1 r -> 1
-            float Xn = (2 * Xp) / (r - l) - (r + l) / (r - l);
-            //Yp -> Yn : b -> -1 t -> 1
-            float Yn = (2 * Yp) / (t - b) - (t + b) / (t - b);
-
-            float Zn = (- ((f + n) / (f - n) *  Ze) - (2 * f * n) / (f - n)) / -Ze;
-
-            oPoint.x = Xn;
-            oPoint.y = Yn;
-            oPoint.z = Zn;
-            return oPoint;
-        };
-        vector<PointCoord> vproject;
-#if 1
-//calculate test
-        for(auto& vertex : vertex1)
-        {
-            vproject.emplace_back(Perspective(vertex.x, vertex.y, vertex.z));
-        }
-        vertex1.swap(vproject);
-        vproject.clear();
-        for(auto& vertex : vertex2)
-        {
-            vproject.emplace_back(Perspective(vertex.x, vertex.y, vertex.z));
-        }
-        vertex2.swap(vproject);
-#else
-//multiply matrix test
-        glm::mat4 frustumMatrix = glm::frustum(l, r, b, t, n, f);
-        for(auto & vertex : vertex1)
-        {
-            PointCoord p;
-            glm::vec4 vp = frustumMatrix * glm::vec4(vertex.x, vertex.y, vertex.z, 1.f);
-            p.x = vp.x / vp.w;
-            p.y = vp.y / vp.w;
-            p.z = vp.z / vp.w;
-            vproject.emplace_back(p);
-        }
-        vertex1.swap(vproject);
-        vproject.clear();
-        for(auto& vertex : vertex2)
-        {
-            PointCoord p;
-            glm::vec4 vp = frustumMatrix * glm::vec4(vertex.x, vertex.y, vertex.z, 1.f);
-            p.x = vp.x / vp.w;
-            p.y = vp.y / vp.w;
-            p.z = vp.z / vp.w;
-            vproject.emplace_back(p);
-        }
-        vertex2.swap(vproject);
-#endif
+        glDepthFunc(GL_LEQUAL);
 //matrix
-        //projectMatrix = glm::frustum(l, r, b, t, n, f);
-        //projectMatrix = glm::ortho(l, r, b, t, n, f);
+        glm::mat4 mvp(1.0);
+        glm::mat4 world(1.0);
+        glm::mat4 frustum(1.0);
+
+        float cameraDistance = 500.f;
+        float surveyAngle = 0;
+        float rFov = std::tan((FOV * 3.14159265 / 180.0) / 2.0);
+        float aspectRatio = WINDOW_WIDTH / WINDOW_HEIGHT;
+        Point2D screenCenterPos(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
 
 
+        float frustumNear = 0.1f;
+        float frustumFar = 1000.f;
+        float frustumLeft = -frustumNear * rFov * aspectRatio;
+        float frustumRight = frustumNear * rFov * aspectRatio;
+        float frustumBottum = -frustumNear * rFov;
+        float frustumTop = frustumNear * rFov;
 
+        float EyeDistanceY = 0;
+        float EyeDistanceZ = cameraDistance;
+
+        frustum = glm::frustum(frustumLeft, frustumRight, frustumBottum, frustumTop, frustumNear, frustumFar);
+
+        world = glm::translate(world, glm::vec3(0.f, EyeDistanceY, -EyeDistanceZ));
+        world = glm::rotate(world, glm::radians(-surveyAngle), glm::vec3(1.f, 0.f, 0.f));
+
+        mvp = frustum * world;
 //render
-        shader.SetMatrixValue("modelMatrix", glm::value_ptr(modelMatrix));
-        shader.SetMatrixValue("viewMatrix", glm::value_ptr(viewMatrix));
-        shader.SetMatrixValue("projectMatrix", glm::value_ptr(projectMatrix));
+        shader.SetMatrixValue("mvp", glm::value_ptr(mvp));;
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
 
-        unsigned int VBOvertex1;
-        glGenBuffers(1, &VBOvertex1);
-        glBindBuffer(GL_ARRAY_BUFFER, VBOvertex1);
-        glBufferData(GL_ARRAY_BUFFER, vertex1.size() * sizeof(vertex1[0]), (void*)&vertex1[0], GL_DYNAMIC_DRAW);
-
+        glBindBuffer(GL_ARRAY_BUFFER, VBOvertex);
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex1[0]), (void*)0);
-
-        unsigned int VBOtextureCoord;
-        glGenBuffers(1, &VBOtextureCoord);
-        glBindBuffer(GL_ARRAY_BUFFER, VBOtextureCoord);
-        glBufferData(GL_ARRAY_BUFFER, textureCoord.size() * sizeof(textureCoord[0]), (void*)&textureCoord[0], GL_DYNAMIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex[0]), (void*)0);
         glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(textureCoord[0]), (void*)0);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, vertex1.size());
-
-        glBindTexture(GL_TEXTURE_2D, texture2);
-        unsigned int VBOvertex2;
-        glGenBuffers(1, &VBOvertex2);
-        glBindBuffer(GL_ARRAY_BUFFER, VBOvertex2);
-        glBufferData(GL_ARRAY_BUFFER, vertex2.size() * sizeof(vertex2[0]), (void*)&vertex2[0], GL_DYNAMIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex2[0]), (void*)0);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, vertex2.size());
-
-        glBindTexture(GL_TEXTURE_2D, 0);
-        glDeleteBuffers(1, &VBOtextureCoord);
-        glDeleteBuffers(1, &VBOvertex1);
-        glDeleteBuffers(1, &VBOvertex2);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(vertex[0]), (void*)(3 * sizeof (float)));
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, vertex.size());
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -380,11 +262,11 @@ void WindowSizeCallback(GLFWwindow* window, int width, int height)
 
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
 {
-  if(fov >= 1.0f && fov <= 45.0f)
-    fov -= yoffset;
-  if(fov <= 1.0f)
-    fov = 1.0f;
-  if(fov >= 45.0f)
-    fov = 45.0f;
+  if(FOV >= 1.0f && FOV <= 45.0f)
+    FOV -= yoffset;
+  if(FOV <= 1.0f)
+    FOV = 1.0f;
+  if(FOV >= 45.0f)
+    FOV = 45.0f;
 }
 
