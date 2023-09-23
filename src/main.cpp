e0475679d044d9b76acf99710b458fb73650dd9d
cuifh <cuifh@meixing.com>
2023-09-23 17:12:09 +0800

    fs0923

--- src/main.cpp
+++ src/main.cpp
@@ -3,67 +3,37 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 
-void CheckShader(GLuint shader);
-void CheckProgram(GLuint program);
-
+bool InitWindow(GLFWwindow*& window);
+bool InitShader(GLuint& shderProgram);
+void MakeRenderData(GLuint& dataVBO);
+void Display(GLFWwindow* window, GLuint shderProgram, GLuint dataVBO);
 int main(int argc, char* agrv[])
 {
-    //init
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    auto window = glfwCreateWindow(800, 600, "HAHA", nullptr, nullptr);
-    if (window == nullptr)
+    GLFWwindow* window = nullptr;
+    if(!InitWindow(window))
     {
-        glfwTerminate();
         return 0;
     }
-    glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    GLuint shderProgram = 0;
+    if(!InitShader(shderProgram))
     {
-        glfwTerminate();
         return 0;
     }
-    //shader
-    const char* vss =
-        "void main()"
-        "{"
-        "   gl_Position = vec4(0.0, 0.0, 0.0, 1.0);"
-        "}";
-    const char* fss =
-        "void main()"
-        "{"
-        "   gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);"
-        "}";
-    auto vs = glCreateShader(GL_VERTEX_SHADER);
-    auto fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(vs, 1, &vss, nullptr);
-    glShaderSource(fs, 1, &fss, nullptr);
-    glCompileShader(vs);
-    CheckShader(vs);
-    glCompileShader(fs);
-    CheckShader(fs);
-    auto program = glCreateProgram();
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    CheckProgram(program);
-    //VAO
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-
+    GLuint dataVBO = 0;
+    MakeRenderData(dataVBO);
     while(!glfwWindowShouldClose(window))
     {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glUseProgram(program);
-        glPointSize(30.f);
-        glDrawArrays(GL_POINTS, 0, 1);
+        Display(window, shderProgram, dataVBO);;
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -71,32 +41,150 @@ int main(int argc, char* agrv[])
     return 0;
 }
 
-void CheckShader(GLuint shader)
+void Display(GLFWwindow *window, GLuint shderProgram, GLuint dataVBO)
 {
-    GLint size = 0;
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &size);
-    if(size > 0)
-    {
-        char* info = new char[size];
-        std::memset(info, 0x0, size);
-        GLsizei realSize = 0;
-        glGetShaderInfoLog(shader, size, &realSize, info);
-        std::cout << info << std::endl;
-        delete [] info;
-    }
+    glUseProgram(shderProgram);
+    glm::mat4 matrix_model(1.f);
+    matrix_model = glm::translate(matrix_model, glm::vec3(0.f, -2.f, 0.f));
+    glUniformMatrix4fv(glGetUniformLocation(shderProgram, "matrix_model"), 1, GL_FALSE, glm::value_ptr(matrix_model));
+
+    glm::mat4 matrix_view(1.f);
+    matrix_view = glm::translate(matrix_view, glm::vec3(0.f, 0.f, -8.f));
+    glUniformMatrix4fv(glGetUniformLocation(shderProgram, "matrix_view"), 1, GL_FALSE, glm::value_ptr(matrix_view));
+
+    glm::mat4 matrix_project(1.f);
+    int windowWidth = 0;
+    int windowheight = 0;
+    glfwGetFramebufferSize(window, &windowWidth, &windowheight);
+    float aspect = (float)windowWidth / (float)windowheight;
+    matrix_project = glm::perspective(glm::radians(60.f), aspect, 0.1f, 1000.0f);
+    glUniformMatrix4fv(glGetUniformLocation(shderProgram, "matrix_project"), 1, GL_FALSE, glm::value_ptr(matrix_project));
+
+    glBindBuffer(GL_ARRAY_BUFFER, dataVBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
-void CheckProgram(GLuint program)
+void MakeRenderData(GLuint& dataVBO)
 {
-    GLint size = 0;
-    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &size);
-    if(size > 0)
+    float vertexPositions[108] = {
+        -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f
+    };
+    glGenBuffers(1, &dataVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, dataVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+}
+
+
+bool InitWindow(GLFWwindow*& window)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window = glfwCreateWindow(600, 300, "HAHA", nullptr, nullptr);
+    if (window == nullptr)
     {
-        char* info = new char[size];
-        std::memset(info, 0x0, size);
-        GLsizei realSize = 0;
-        glGetProgramInfoLog(program, size, &realSize, info);
-        std::cout << info << std::endl;
-        delete [] info;
+        glfwTerminate();
+        return false;
     }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        glfwTerminate();
+        return false;
+    }
+    return true;
+}
+
+bool InitShader(GLuint& shderProgram)
+{
+    const char* vss =
+        "#version 430\n"
+        "layout (location = 0) in vec3 position;"
+        "uniform mat4 matrix_model;"
+        "uniform mat4 matrix_view;"
+        "uniform mat4 matrix_project;"
+        "void main()"
+        "{"
+        "   gl_Position = matrix_project * matrix_view * matrix_model * vec4(position, 1.0);"
+        "}";
+    const char* fss =
+        "#version 430\n"
+        "out vec4 color;"
+        "void main()"
+        "{"
+        "   color = vec4(0.0, 0.0, 1.0, 1.0);"
+        "}";
+    auto CheckShader = [](GLuint shader)
+    {
+        GLint size = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &size);
+        if(size > 0)
+        {
+            char* info = new char[size];
+            std::memset(info, 0x0, size);
+            GLsizei realSize = 0;
+            glGetShaderInfoLog(shader, size, &realSize, info);
+            std::cout << info << std::endl;
+            delete [] info;
+            return false;
+        }
+        return true;
+    };
+    auto CheckProgram = [](GLuint program)
+    {
+        GLint size = 0;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &size);
+        if(size > 0)
+        {
+            char* info = new char[size];
+            std::memset(info, 0x0, size);
+            GLsizei realSize = 0;
+            glGetProgramInfoLog(program, size, &realSize, info);
+            std::cout << info << std::endl;
+            delete [] info;
+            return false;
+        }
+        return true;
+    };
+    auto vs = glCreateShader(GL_VERTEX_SHADER);
+    auto fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(vs, 1, &vss, nullptr);
+    glShaderSource(fs, 1, &fss, nullptr);
+    glCompileShader(vs);
+    if(!CheckShader(vs))
+    {
+        std::cout << "vs error !!!!!!!!" << std::endl;
+        return false;
+    }
+    glCompileShader(fs);
+    if(!CheckShader(fs))
+    {
+        std::cout << "fs error !!!!!!!!" << std::endl;
+        return false;
+    }
+    shderProgram = glCreateProgram();
+    glAttachShader(shderProgram, vs);
+    glAttachShader(shderProgram, fs);
+    glLinkProgram(shderProgram);
+    if(!CheckProgram(shderProgram))
+    {
+        return false;
+    }
+    return true;
 }
